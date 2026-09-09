6f8b87f11a7fa983cb1826174056254cfd81d0d7
zacharyb12 <boualemzachary89@yahoo.fr>
2026-09-09 15:15:53 +0200

    demo list

--- exo/exo1.py
+++ exo/exo1.py
@@ -1,42 +1,125 @@
-#1 . 
-# # jeu du nombre mystère
+import random
+
+menu = """
+nombre mystère        : 1
+FizzBuzz              : 2
+Pierre Papier Ciseaux : 3
+Quitter               : 4
 """
 
-- générer un nombre aléatoire : 
-    import random 
+stop_loop = False
 
-    random.randint(0,100)
+while not stop_loop:
+    
+    print(menu)
+    
+    choice = input("Entrez votre choix : ")
+    
+    match choice:
+        # Nombre mystère
+        case "1":
+            print('mystery')
+            mystery = random.randint(0,101)
+            victory = False
 
-demander un nombre à l'utilisateur 
-si le nombre est plus grand afficher un message spécifique
-si le nombre est plus petit afficher un message spécifique
-si le nombre est correct afficher un message mettre fin au jeu
-"""
+            while not victory:
+                choice = int(input("Entrez un nombre entre 0 et 100 : "))
+                
+                if choice < mystery:
+                    print('le nombre mystere est plus grand')
+                elif choice > mystery:
+                    print('le nombre mystère est plus petit')
+                else:
+                    print(f'Bravo vous avez trouver le nombre mystere : {mystery}')
+                    victory = True
+        # FizzBuzz
+        case "2":
+            print('FizzBuzz')
+            value = int(input("Combien de valeur voulez-vous afficher ? "))
 
-#2 . =========================================================
-#FizzBuzz
-"""
-fizzbuzz : range() : 
-Afficher « FizzBuzz » si le nombre est un multiple de 3 et de 5 (divisible par 15).
-Afficher « Fizz » si le nombre est un multiple de 3.
-Afficher « Buzz » si le nombre est un multiple de 5
-Afficher le nombre lui-même dans tous les autres cas.
-"""
+            for i in range(1 , value + 1):
+                
+                if i % 3 == 0 and i % 5 == 0:
+                    print("FizzBuzz")
+                    
+                elif i % 3 == 0:
+                    print('Fizz')
+                    
+                elif i % 5 == 0:
+                    print("Buzz")
+                    
+                else:
+                    print(i)
+        # Pierre Papier Ciseaux
+        case "3":
+            print('Pierre papier ciseaux')
+            bot_points = 0
+            user_points = 0
 
-# 3 . ========================================================= 
-# Programme interactif
-"""gérer un programme qui propose à l'utilisateur les actions disponibles
-    # dans une boucle while
-    # proposer les choix
-    # récupérer le choix de l'utilisateur
-    # executer le code correspondant
-    # prévoir l'arrèt du programme
-"""
+            bot_win = False
+            user_win = False
 
-# 4 . =========================================================
-# Pierre papier ciseaux
-"""
-valeur générer aléatoirement pour l'ordinateur
-comparaison du resultat
-le premier à trois point gagne
-"""
+            indications = """
+            Pierre  1
+            Papier  2
+            Ciseaux 3
+            """
+
+
+            while bot_points < 3 and user_points < 3 :
+                bot_choice = random.randint(1,3)
+                user_choice = input(indications)
+                
+                print(f"Bot : {bot_choice} - user : {user_choice}")
+                match bot_choice :
+                    case 1:# Pierre
+                        match user_choice :
+                            case "1": # Pierre
+                                print("égalité")
+                                
+                            case "2": # Papier
+                                print('Victoire')
+                                user_points += 1
+                            case "3": # Ciseaux
+                                print('Défaite')
+                                bot_points += 1
+                    case 2 :# Papier
+                        match user_choice :
+                            case "1":# Pierre
+                                print('Défaite')
+                                bot_points += 1
+                                
+                            case "2":# Papier
+                                print("égalité")
+                                
+                            case "3":# Ciseaux
+                                print('Victoire')
+                                user_points += 1
+                    case 3 : # Ciseaux
+                        match user_choice :
+                            case "1": # Pierre
+                                print('Victoire')
+                                user_points += 1
+                                
+                            case "2": # Papier
+                                print('Défaite')
+                                bot_points += 1
+                            case "3": # ciseaux
+                                print("égalité")
+                
+                if user_points == 3:
+                    user_win = True
+                elif bot_points == 3:
+                    bot_win = True
+                
+                if bot_points == 3 or user_points == 3:
+                    if bot_win :
+                        print(f'La machine à gagné , vous aviez {user_points} points')
+                    if user_win :
+                        print(f'Vous avez gagné , La machine avait {bot_points} points')
+            
+        case "4":
+            stop_loop = True
+    
+    if stop_loop == True:
+        print("Merci à bientot")
