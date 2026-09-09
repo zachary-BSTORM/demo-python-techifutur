8d8d9d9bf65a6d7a8f6207624cdb896a3db30c0f
zacharyb12 <boualemzachary89@yahoo.fr>
2026-09-09 11:00:38 +0200

    exo-1

--- intro.py
+++ intro.py
@@ -1,6 +1,18 @@
 
-    
-compteur = 10
+#1 . jeu du nombre mystère
 
-while compteur < 10:
-    print("le compteur est inférieur à 10" )
+
+#2 . fizzbuzz : range() : 
+"""
+Afficher « FizzBuzz » si le nombre est un multiple de 3 et de 5 (divisible par 15).
+Afficher « Fizz » si le nombre est un multiple de 3.
+Afficher « Buzz » si le nombre est un multiple de 5
+Afficher le nombre lui-même dans tous les autres cas.
+"""
+
+# 3 . gérer un programme qui propose à l'utilisateur les actions disponibles
+    # dans une boucle while
+    # proposer les choix
+    # récupérer le choix de l'utilisateur
+    # executer le code correspondant
+    # prévoir l'arrèt du programme
