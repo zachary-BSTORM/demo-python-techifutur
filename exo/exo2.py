111131ba9cc963bcd6cbd8b5251777dcd67b2a0c
zacharyb12 <boualemzachary89@yahoo.fr>
2026-09-10 12:16:47 +0200

    correction exo 2

--- exo/exo2.py
+++ exo/exo2.py
@@ -1,32 +1,4 @@
-
-# Calcul moyenne de température
-"""
-Calculez la somme et la moyenne des valeurs d'une liste,
-temperatures = [18.5, 21.0, 19.75, 23.5, 17.25, 20.0, 22.5]
-"""
-
-# Inversion d'une liste
-"""
-Inverser une liste Construisez une nouvelle liste contenant les éléments de la liste d'origine dans l'ordre inverse
-langages = ["Python", "C#", "JavaScript", "SQL", "TypeScript"]
-"""
-# Supprimer les doublons Créez une nouvelle liste sans doublons
-"""
-visiteurs = ["alice", "bob", "alice", "carol", "bob", "dave", "alice"]
-"""
-
-# Fusion de dictionnaire 
-"""
-Fusionner deux stocks Deux entrepôts envoient leur inventaire. 
-Construisez un dictionnaire du stock total : 
-les produits communs voient leurs quantités additionnées, 
-les produits présents dans un seul entrepôt sont repris tels quels.
-
-entrepot_a = {"clavier": 12, "souris": 30, "ecran": 5}
-entrepot_b = {"souris": 15, "ecran": 8, "webcam": 7, "casque": 3}
-"""
-
-# Programme intercatif
+# Programme interactif
 """
 Le panier de courses Écrivez un programme avec un menu qui boucle 
 jusqu'à ce que l'utilisateur choisisse de quitter.
@@ -38,3 +10,53 @@ jusqu'à ce que l'utilisateur choisisse de quitter.
 5. Quitter
 Votre choix :
 """
+
+menu = """
+1. Ajouter un produit
+2. Retirer un produit
+3. Afficher le panier
+4. Vider le panier
+5. Quitter
+"""
+panier = {}
+
+stop_app = False
+
+while not stop_app:
+    print("==========================================")
+    print(menu)
+    
+    choice = input("Entrez votre choix : ")
+    
+    match choice:
+        case "1":
+            key = input("Entrez l'identifiant du produit : ")
+            
+            name = input("Entrez le nom du produit : ")
+            price = float(input("Entrez le prix du produit : "))
+            
+            panier[key] = {"name":name,"price":price}
+            
+            
+        case "2":
+            key = input("Entrez l'identifiant du produit à supprimer : ")
+            
+            if key in panier.keys():
+                del panier[key]
+            else:
+                print("ce produit n'est pas dans le stock!")
+                
+                
+        case "3":
+            for key,value in panier.items():
+                print(f"{key} : name : {value["name"]} - price : {value["price"]}€")
+            
+        case "4":
+            panier.clear()
+            
+        case "5":
+            stop_app = True
+            
+    
+    if stop_app == True:
+        print("Merci à bientot !")
