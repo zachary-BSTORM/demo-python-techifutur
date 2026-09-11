a04c4862d356cd588385d3cce23ead95b7ccde0e
zacharyb12 <boualemzachary89@yahoo.fr>
2026-09-11 08:33:00 +0200

    ajout lien enquete satisfaction

--- exo/exo3.py
+++ exo/exo3.py
@@ -1,8 +1,15 @@
+# import
+
+# declarations des variables
+
+# methodes
+
+# logique
 
 # Gestion d'utilisateur
 
 """
-les utilisateurs sont dans une dict
+les utilisateurs sont dans un dict
 
 "model" : 
     - name
@@ -25,3 +32,55 @@ methodes :
     - récupération du choix 
     - execution
 """
+
+list_user = dict()
+
+menu = """
+Afficher liste        : 1
+Ajouter utilisateur   : 2
+Modifier utilisateur  : 3
+Supprimer utilisateur : 4
+Quitter               : 5
+"""
+
+
+def get_name():
+    pass
+
+def get_number():
+    pass
+
+def get_role():
+    pass
+
+def add_user(users):
+    pass
+
+def remove_user(users):
+    pass
+
+def update_user(users,updated_users):
+    pass
+
+def show_user(users):
+    pass
+
+
+stop_app : False
+
+while not stop_app:
+    print(menu)
+    
+    choice = input("Entrez votre choix : ")
+    
+    match choice :
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            pass
+        case "5":
+            pass
