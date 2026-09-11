f55f16c553cc511e06a6802c14110c4cd006e6d6
zacharyb12 <boualemzachary89@yahoo.fr>
2026-09-11 15:39:31 +0200

    correction exo oo

--- exo/exo4.py
+++ exo/exo4.py
@@ -37,20 +37,171 @@ suppression_vehicule()
 
 """
 
-class MaClasse():
-    def __init__(self,prop1):
-        self.prop1 = prop1
+# class MaClasse():
+#     def __init__(self,prop1):
+#         self.prop1 = prop1
         
         
+#     @property
+#     def name(self):
+#         return self.__name
+    
+#     @name.setter
+#     def name(self,value):
+#         self.__name = value 
+    
+        
+
+
+# ============================================================================
+
+class Vehicule():
+    """
+    id
+    couleur
+    marque
+    puissance
+    nb_place
+
+    deplacement() : affiche un message
+    """
+    def __init__(self,id,couleur,marque,puissance,nb_place):
+        self.id = id
+        self.couleur = couleur
+        self.marque = marque
+        self.puissance = puissance
+        self.nb_place = nb_place
+        
+    def deplacement(self):
+        return "le vehicule se déplace"
+    
+
+class Voiture(Vehicule):
+    """
+    nb_roues
+    carburant
+
+    deplacement() : récupère le comportement du parent et ajoute la voiture roule       
+    """
+    def __init__(self,id,couleur,marque,puissance,nb_place,nb_roues,carburant):
+        super().__init__(id,couleur,marque,puissance,nb_place)
+        self.nb_roues = nb_roues
+        self.carburant = carburant
+        
+    def deplacement(self):
+        print(super().deplacement())
+        print("la voiture roule")
+
     @property
-    def name(self):
-        return self.__name
+    def carburant(self):
+        return self.__carburant
+
+    @carburant.setter
+    def carburant(self,value):
+        if value in ["diesel","essence"]:
+            self.__carburant = value
+        else:
+            self.__carburant = "inconnu"
+
+
+class Bateau(Vehicule):
+    """
+    possede_moteur
+    type : "plaisance - peche - sportif"
     
-    @name.setter
-    def name(self,value):
-        self.__name = value 
+    deplacement() : récupère le comportement du parent et ajoute le bateau navigue
+    """
     
+    def __init__(self,id,couleur,marque,puissance,nb_place,motor,type_boat):
+        super().__init__(id,couleur,marque,puissance,nb_place)
+        self.motor = motor
+        self.type = type_boat
         
+    def deplacement(self):
+        print(super().deplacement())
+        print("le bateau navigue")
+
+class Concession():
+    def __init__(self,name):
+        self.name = name
+        self.vehicules = dict()
+        self.lastId = 0
         
+    def ajout_vehicule(self,new_vehicule):
+            self.vehicules[self.lastId] = new_vehicule
+            self.lastId += 1
+
         
-obj_1 = MaClasse("info 1")
+    def remove_vehicule(self,key):
+        if key in self.vehicules.keys():
+            del self.vehicules[key]
+        else:
+            print("Aucun élément avec cet id ")
+    
+    def show_vehicles(self):
+        for k,v in self.vehicules.items():
+            print(f"key : {k} : id : {v.id} - couleur : {v.couleur} - marque : {v.marque} - puissance : {v.puissance} - nb_places : {v.nb_place}")
+
+def get_vehicle():
+        type_vehicule = input("Quel type de vehicule voulez-vous ajouter ? b : bateau - v : voiture")
+        id = int(input("Entrez l'id du vehicule : "))
+        color = input("Entrez la couleur du vehicule : ")
+        brand = input("Entrez la marque du vehicule")
+        hp = int(input("Entrez la puissance du vehicule : "))
+        nb_place = input("Entrez le nombre de place du vehicule : ")
+        
+        return type_vehicule,id,color,brand,hp,nb_place
+
+menu = """
+afficher les vehicules 1
+ajouter un vehicule    2
+supprimer un vehicule  3
+Quitter                4
+"""
+
+concession = Concession("ma_concession")
+
+
+while True:
+    print("==========================")
+    print(menu)
+    
+    choice = input("Entrez votre choix : ")
+    
+    match choice:
+        case "1":
+            concession.show_vehicles()
+        
+        case "2":
+            type_vehicule,id,color,brand,hp,nb_place = get_vehicle()
+            
+            if type_vehicule == "b":
+                motor = input("Est-ce que le bateau possède un moteur ? y - n")
+                motor_boat = True if motor == "y" else False
+                boat_type = input("Quel est le type du bateau ? plaisance - peche - croisière :")
+                
+                new_boat = Bateau(id,color,brand,hp,nb_place,motor_boat,boat_type)
+                concession.ajout_vehicule(new_boat)
+                
+                
+                
+            elif type_vehicule == "v":
+                nb_wheels = int(input("Combien de roues possède le vehicule ? "))
+                fuel = input("Quel est le type de carburant du vehicule? ")
+                new_car = Voiture(id,color,brand,hp,nb_place,nb_wheels,fuel)
+                
+                concession.ajout_vehicule(new_car)
+            else :
+                print("Il n'est pas possible d'ajouter d'autres types de vehicules pour le moment")
+            
+        case "3":
+            print("=====================================")
+            for k,v in concession.vehicules.items():
+                print(f"{k} - {v.marque}")
+            print("=====================================")
+            
+            id_delete = int(input("Quel vehicule voulez-vous supprimez ? "))
+            
+            concession.remove_vehicule(id_delete)
+        case "4":
+            break
--- intro.py
+++ intro.py
@@ -1,9 +1,7 @@
 class Animal():
     def __init__(self,nom : str,age : int,couleur : str):
         self.nom = nom
-        self._age = age
         self.couleur = couleur
-        
         
     @property
     def couleur(self):
@@ -11,65 +9,15 @@ class Animal():
     
     @couleur.setter
     def couleur(self,value):
-        if value is not None:
-            if len(value) < 3:
-                print("le nom doit faire au minimum 3 caractères")
-            else:
-                self.__couleur = value
+        self.__couleur = value
         
     def faire_du_bruit(self):
         print("l'animal fait du bruit")
-
-class Chat(Animal):
-    """
-    la classe chat récupère son comportement de son parent : Animal
-    """
-    
-    def __init__(self,nom,age,croquettes,couleur):
-        super().__init__(nom,age,couleur)
-        self.croquettes = croquettes
-        
-    def faire_du_bruit(self):
-        super().faire_du_bruit()
-        print("miaou du chat")
-
-class Chaton(Chat):
-    """
-    ici la classe chaton récupère faire du bruit de son parent le plus proche : Chat
-    """
-    def __init__(self,jouet,nom,age,croquettes,couleur):
-        super().__init__(nom,age,croquettes,couleur)
-        self.jouet = jouet
-    
-    pass
-
-
-class Animalerie():
-    def __init__(self,nom):
-        self.nom = nom
-        self.animaux = []
-
-    def ajouter_animal(self,new_animal):
-        self.animaux.append(new_animal)
         
         
-    def liste_animaux(self):
-        for a in self.animaux:
-            if isinstance(a,Chaton):
-                print(f" l'animal {a.nom} - age {a._age} - couleur {a.couleur} - jouet {a.jouet}")
-            else:
-                print(f" l'animal {a.nom} - age {a._age} - couleur {a.couleur}")
 
-# ===========================================================================
-rex = Animal("rex",8,"noi et brun")
-garfield = Chat("garfield",5,"whiskas","roux")
-mini_garfield = Chaton("pelotte de laine","bébé garfield",1,"whiskas","roux")
+animal_1 = Animal("garfield",5,"roux")
 
-tom_and_co = Animalerie("animalerie")
+print(animal_1.couleur)
+print(animal_1._Animal__couleur)
 
-
-tom_and_co.ajouter_animal(rex)
-tom_and_co.ajouter_animal(garfield)
-tom_and_co.ajouter_animal(mini_garfield)
-
-tom_and_co.liste_animaux()
